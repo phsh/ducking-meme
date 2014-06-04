e8086b23b3848248fed8d6ce53559271cc5a38d5
Per Hedman <per.hedman@unibet.com>
2014-06-04 15:17:19 +0200

    added more output

--- ChildGame.py
+++ ChildGame.py
@@ -12,12 +12,17 @@ Question("Vad är 4 + 6 ?","10"),
 Question("Vad är 4 + 8 ?","12"), 
 Question("Vad är 1 + 13 ?","14") 
 ]
-
+antal_correct = 0
 for question in questions:
 	answer = raw_input(question.question + " :")
 	if answer == question.answer:
+		antal_correct += 1
 		print "Rätt"
 	else:
 		print "Fel"
+
+print "------------------"
+print "Du hade %s rätt utav %s frågor" % (antal_correct, len(questions))
+print "------------------"
 
 
