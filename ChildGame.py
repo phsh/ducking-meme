e8de3f6762dbd0ea39c589332d828511eeb8a583
Per Hedman <per.hedman@unibet.com>
2014-06-04 16:31:17 +0200

    minor changes and improvements, strip and fault tolerans

--- ChildGame.py
+++ ChildGame.py
@@ -7,9 +7,13 @@ class Question:
 		self.question = question
 		self.answer = answer
 
+print "=========" * 10
+print "Frågesport"
+print "=========" * 10
+
 aLotOfQuestions = {
 	" 2 +  6 ? ": "8", " 4 +  6 ? ": "10",
-	" 4 +  8 ? ": "10"," 1 + 13 ? ": "14",
+	" 4 +  8 ? ": "12"," 1 + 13 ? ": "14",
 	" s ä t H ?": "Häst", " r s i G ?": "Gris",
 	" 2 +  5 ?": "7", " R s o ?": "Ros",
 	" o B k ?": "Bok"
@@ -27,14 +31,15 @@ questionsFile.close()
 antal_correct = 0
 for question in questions:
 	answer = raw_input(question.question + " :")
-	if answer.lower() == question.answer.lower():
+	if answer.lower().strip() == question.answer.lower().strip():
 		antal_correct += 1
 		print "Rätt"
 	else:
 		print "Fel"
 
-print "------------------"
+print "=========" * 10
 print "Du hade %s rätt utav %s frågor" % (antal_correct, len(questions))
-print "------------------"
-
-
+print "=========" * 10
+if len(questions)-antal_correct <= 1:
+	print "GULDSTJÄRNA!!!"
+	print "=========" * 10
