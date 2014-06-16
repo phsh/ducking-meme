77851204de0859a26528c9c55dd8fb07d1a3f1cf
Per Hedman <phhedman@gmail.com>
2014-06-16 14:45:46 +0200

    added a unittest

--- Question.py
+++ Question.py
@@ -1,7 +1,23 @@
 # _author_ Per Hedman
+import unittest
+
 class Question:
 	
 	def __init__(self, question, answer, level = 0):
 		self.question = question
 		self.answer = answer
 		self.level = level
+
+	def isCorrect(self, my_answer):
+		return self.answer.lower().strip() == my_answer.lower().strip()
+
+class TestQuestion(unittest.TestCase):
+	def setUp(self):
+		self.question = Question('4 + 4 ?','8')
+
+	def test_question(self):
+		self.assertTrue(self.question.isCorrect('8'))
+		self.assertTrue(self.question.isCorrect('8 '))
+
+if __name__  =='__main__':
+	unittest.main()
