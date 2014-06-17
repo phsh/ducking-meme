1ae976be4904c335c046956ba4246b5c3dfa4224
Per Hedman <phhedman@gmail.com>
2014-06-17 13:15:24 +0200

    added another unittest

--- TestQuestion.py
+++ TestQuestion.py
@@ -2,24 +2,38 @@
 import unittest
 from Question import Question
 from QuestionGenerator import QuestionGenerator
+
 class TestQuestion(unittest.TestCase):
-	
-	def setUp(self):
-		self.wordList = ['kalle']
-		self.question = Question('4 + 4 ?','8')
-		self.generatedQuestion = QuestionGenerator.generateWordQuestion(self.wordList)
-
-	def test_true_question(self):
-		self.assertTrue(self.question.isCorrect('8'))
-		self.assertTrue(self.question.isCorrect('8 '))
-
-	def test_true_generated_question(self):
-		self.assertTrue(self.generatedQuestion.isCorrect(self.generatedQuestion.answer))
-		self.assertTrue(self.generatedQuestion.isCorrect(self.wordList[0]))
+	wordList = ['kalle']	
 		
+	def test_question_true(self):
+		question = Question('4 + 4 ?','8')
+		self.assertTrue(question.isCorrect('8'))
+		self.assertTrue(question.isCorrect('8 '))
+		self.assertTrue(question.isCorrect(question.answer));
+
 	def test_false_question(self):
-		self.assertFalse(self.question.isCorrect('Kalle Anka'))
-		self.assertFalse(self.question.isCorrect('9'))
+		question = Question('4 + 4 ?','8')
+		self.assertFalse(question.isCorrect('Kalle Anka'))
+		self.assertFalse(question.isCorrect('9'))
+
+	def test_generate_word_question_true(self):
+		generatedQuestion = QuestionGenerator.generateWordQuestion(self.wordList)
+		self.assertTrue(generatedQuestion.isCorrect(generatedQuestion.answer))
+		self.assertTrue(generatedQuestion.isCorrect(self.wordList[0]))
+
+	def test_generate_word_question_false(self):
+		generatedQuestion = QuestionGenerator.generateWordQuestion(self.wordList)
+		self.assertFalse(generatedQuestion.isCorrect(''))
+		self.assertFalse(generatedQuestion.isCorrect('22'))		
+
+	def test_generate_math_question_true(self):
+		question = QuestionGenerator.generateMathQuestion(1,1)
+		self.assertTrue(question.isCorrect('2'))
+
+	def test_generate_math_question_false(self):
+		question = QuestionGenerator.generateMathQuestion(1,1)
+		self.assertTrue(question.isCorrect('2'))
 
 if __name__  =='__main__':
 	unittest.main()
