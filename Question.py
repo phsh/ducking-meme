edf1cbd61eaa3d7169ac2ed0bccf1ea3a514ec8c
Per Hedman <phhedman@gmail.com>
2014-06-18 13:53:43 +0200

    Extracted two subclasses

--- Question.py
+++ Question.py
@@ -3,10 +3,17 @@ import unittest
 
 class Question:
 	
-	def __init__(self, question, answer, level = 0):
+	def __init__(self, question, answer):
 		self.question = question
 		self.answer = answer
-		self.level = level
 
 	def isCorrect(self, my_answer):
 		return self.answer.lower().strip() == my_answer.lower().strip()
+
+class MathQuestion(Question):
+	def __init__(self,question,answer):
+		Question.__init__(self,question,answer)
+
+class WordQuestion(Question):
+	def __init__(self,question,answer):
+		Question.__init__(self,question,answer)	
